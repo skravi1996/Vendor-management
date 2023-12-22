b51de7ef2d1a12cf4b3898c7ee43a2a9b3aea54c
sk.ravi1996 <sk.ravi1996.com>
2023-12-23 01:48:32 +0530

    First commit add the vendor management project

---
+++ vendor_api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import (
+    VendorListCreateView,
+    VendorDetailView,
+    PurchaseOrderListCreateView,
+    PurchaseOrderDetailView,
+    VendorPerformanceView,
+    PurchaseOrderAcknowledgmentView,
+    VendorHistoricalPerformanceView,
+    vendor_list,
+)
+
+urlpatterns = [
+    path('vendor/', vendor_list, name='vendor-list'),
+    path('vendors/', VendorListCreateView.as_view(), name='vendor-list-create'),
+    path('vendors/<int:pk>/', VendorDetailView.as_view(), name='vendor-detail'),
+    path('vendors/<int:pk>/performance/',VendorPerformanceView.as_view(), name='vendor-performance'),
+    path('purchase_orders/', PurchaseOrderListCreateView.as_view(),name='purchase-order-list-create'),
+    path('purchase_orders/<int:pk>/', PurchaseOrderDetailView.as_view(),name='purchase-order-detail'),
+    path('purchase_orders/<int:pk>/acknowledge/',PurchaseOrderAcknowledgmentView.as_view(), name='purchase-order-acknowledge'),
+    path('historical_performance/', VendorHistoricalPerformanceView.as_view(),name='vendor-historical-performance'),
+]
